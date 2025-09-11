9842c2be6b44a41f8220538dc183040934324e70
danidanidani11 <dssnuandddani@gmail.com>
2025-09-11 17:22:58 +0330

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ TOKEN = os.getenv("BOT_TOKEN", "8078210260:AAEX-vz_apP68a6WhzaGhuAKK7amC1qUiEY")
 ADMIN_ID = int(os.getenv("ADMIN_ID", 5542927340))
 YOUR_ID = int(os.getenv("YOUR_ID", 123456789))
 DEFAULT_CHANNEL_ID = os.getenv("CHANNEL_ID", "@Charkhoun")
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://charkhon_user:grMZtPEdreHgfbZrmSnrueTjgpvTzdk2@dpg-d2sislggjchc73aeb7og-a/charkhon")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://neondb_owner:npg_qpBCcgGS9d5H@ep-rapid-band-aemkydem-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://0kik4x8alj.onrender.com")
 STRICT_MEMBERSHIP = os.getenv("STRICT_MEMBERSHIP", "true").lower() == "true"
 
